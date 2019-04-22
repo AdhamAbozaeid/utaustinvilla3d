9959895c6086795d88a3c7f4112ed538f3fce982
Adham Abozaeid <adham.abozaeid@gmail.com>
2019-04-21 22:37:32 -0700

    Adjust formatting for defense.cc
    Signed-off-by: Adham Abozaeid <adham.abozaeid@gmail.com>

--- behaviors/defense.cc
+++ behaviors/defense.cc
@@ -12,6 +12,7 @@ still try to block it*/
 #define GOAL_MARGIN 0.5
 
 // Given 2 points on a line, get the slope and intercept
+
 void getLineParam(VecPosition point1, VecPosition point2, double &slope, double &intercept) {
     slope = (point1.getY() - point2.getY()) / (point1.getX() - point2.getX());
     intercept = point1.getY() - (slope * point1.getX());
@@ -27,7 +28,7 @@ bool isBallMoving(const WorldModel *worldModel, double &slope, double &intercept
 
     thisBall.setZ(0);
     lastBall.setZ(0);
-    
+
     if (thisBall.getDistanceTo(lastBall) > 0.2) {
         // the ball moved!
         //cout << "last: " << lastBall << " now: " << thisBall << " Dist: " << thisBall.getDistanceTo(lastBall)<<endl;
@@ -38,12 +39,12 @@ bool isBallMoving(const WorldModel *worldModel, double &slope, double &intercept
         return true;
     }
 
-//    if (thisTime - lastTime < 0.5) {
-//        // not sure yet if the ball has settled
-//        return true;
-//    } else {
-//        return false;
-//    }
+    //    if (thisTime - lastTime < 0.5) {
+    //        // not sure yet if the ball has settled
+    //        return true;
+    //    } else {
+    //        return false;
+    //    }
     return false;
 }
 
@@ -71,49 +72,50 @@ SkillType NaoBehavior::backAction() {
     VecPosition Ball = worldModel->getBall();
     VecPosition goalCenter = VecPosition(-HALF_FIELD_X, 0, 0);
     VecPosition target;
-    double slope, intercept,y, distBallToGoal, distAgentToGoal;
+    double slope, intercept, y, distBallToGoal, distAgentToGoal;
     float angle;
-    
+
     getLineParam(Ball, goalCenter, slope, intercept);
     angle = atan(slope);
     distBallToGoal = abs(Ball.getDistanceTo(goalCenter));
-    
-    
-    if(worldModel->getUNum() == ROLE_BACK_RIGHT) {
-            /*Stand on line few degrees above line from ball to center of goal
-             Back Left will stand at an offset below the line*/
-            angle -= 0.07*(signbit(angle)? -1:1);
-            
-            if(distBallToGoal > 2)
-                distAgentToGoal = 2;
-            else
-                distAgentToGoal = distBallToGoal - 0.01;
-            target.setX(-HALF_FIELD_X + (distAgentToGoal*cos(angle)));
-            y = distAgentToGoal*sin(angle);
-            //cout<<"BR angle: " << angle<<" x: "<<-HALF_FIELD_X + (2*sin(angle))<<" Y: "<<y<<endl;
-            target.setY(y);
+
+
+    if (worldModel->getUNum() == ROLE_BACK_RIGHT) {
+        /*Stand on line few degrees above line from ball to center of goal
+         Back Left will stand at an offset below the line*/
+        angle -= 0.07 * (signbit(angle) ? -1 : 1);
+
+        if (distBallToGoal > 2)
+            distAgentToGoal = 2;
+        else
+            distAgentToGoal = distBallToGoal - 0.01;
+        target.setX(-HALF_FIELD_X + (distAgentToGoal * cos(angle)));
+        y = distAgentToGoal * sin(angle);
+        //cout<<"BR angle: " << angle<<" x: "<<-HALF_FIELD_X + (2*sin(angle))<<" Y: "<<y<<endl;
+        target.setY(y);
     } else {
-            angle += 0.07*(signbit(angle)? -1:1);
+        angle += 0.07 * (signbit(angle) ? -1 : 1);
 
-            if(distBallToGoal > 3)
-                distAgentToGoal = 3;
-            else
-                distAgentToGoal = distBallToGoal - 0.01;
-            target.setX(-HALF_FIELD_X + (distAgentToGoal*cos(angle)));
-            y = distAgentToGoal*sin(angle);
+        if (distBallToGoal > 3)
+            distAgentToGoal = 3;
+        else
+            distAgentToGoal = distBallToGoal - 0.01;
+        target.setX(-HALF_FIELD_X + (distAgentToGoal * cos(angle)));
+        y = distAgentToGoal * sin(angle);
 
-            //cout<<"BL angle: " << angle<<" x: "<<-HALF_FIELD_X + (3*sin(angle))<<" Y: "<<y<<endl;
-            target.setY(y);
+        //cout<<"BL angle: " << angle<<" x: "<<-HALF_FIELD_X + (3*sin(angle))<<" Y: "<<y<<endl;
+        target.setY(y);
     }
 
     target = collisionAvoidance(true /*teammate*/, false/*opponent*/, true/*ball*/, 1/*proximity thresh*/, .5/*collision thresh*/, target, true/*keepDistance*/);
- 
+
     // Use goto relative to face the ball
     return goToTargetRelative(worldModel->g2l(target), angle);
 }
+
 SkillType NaoBehavior::defense() {
 
-    switch(worldModel->getUNum()) {
+    switch (worldModel->getUNum()) {
         case ROLE_GOALIE:
             return goalieAction();
             break;
